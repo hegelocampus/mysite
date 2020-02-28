12eb56179cf6f0ad281242e80c3b9adb7380223c
Bee Ellis <44533466+hegelocampus@users.noreply.github.com>
2020-02-28 15:46:48 -0800

    Fix typos

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,7 @@ from .models import Choice, Question
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    exita = 3
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -23,6 +23,6 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
-    seach_fields = ['question_text']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
